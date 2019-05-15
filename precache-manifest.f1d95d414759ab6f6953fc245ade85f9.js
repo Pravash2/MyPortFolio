8037133e5064a109998d5eb2757eeae4bea537fc
EC2 Default User <ec2-user@ip-172-31-16-203.ec2.internal>
2019-05-15 16:10:15 +0000

    Updates

---
+++ precache-manifest.f1d95d414759ab6f6953fc245ade85f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef494e4215a25bf17385d873f9372f6f",
+    "url": "/MyPortFolio/static/media/profile.ef494e42.png"
+  },
+  {
+    "revision": "0b4647e1aeac4bec1650",
+    "url": "/MyPortFolio/static/js/runtime~main.0b4647e1.js"
+  },
+  {
+    "revision": "7d0752460d8546132935",
+    "url": "/MyPortFolio/static/js/main.7d075246.chunk.js"
+  },
+  {
+    "revision": "147fe8c56b697ff9cdcb",
+    "url": "/MyPortFolio/static/js/1.147fe8c5.chunk.js"
+  },
+  {
+    "revision": "7d0752460d8546132935",
+    "url": "/MyPortFolio/static/css/main.fe031119.chunk.css"
+  },
+  {
+    "revision": "147fe8c56b697ff9cdcb",
+    "url": "/MyPortFolio/static/css/1.06dac2fb.chunk.css"
+  },
+  {
+    "revision": "b7a8e7a7d05752659ce96752d47fb74d",
+    "url": "/MyPortFolio/index.html"
+  }
+];
